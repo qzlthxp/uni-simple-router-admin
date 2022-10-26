0d85e723f31b5e61f37a496f45733ec5b8d8969d
qzlthxp <57819477+qzlthxp@users.noreply.github.com>
2022-10-26 10:24:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,5 @@ export const baseUrl = 'localhost:8080/api'
 
 // 本地存储命名
 export const storageConfig = {
-  userPass: 'USER_PASS', // 保存当前用户登录名和密码（自动登录）
-  userInfo: 'USER_INFO', // 当前用户信息
   userToken: 'USER_TOKEN', // token
-  userList: 'USER_LIST' // 所有用户列表
 }
